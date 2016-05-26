2af505c8a47019712453ccd56dfc45d4a1b063dd
ablount <ablount21@gmail.com>
2016-05-25 20:12:26 -0500

    final email stuff

--- Volunteerism_Abroad/include/sendemail.php
+++ Volunteerism_Abroad/include/sendemail.php
@@ -5,7 +5,7 @@ require_once('phpmailer/PHPMailerAutoload.php');
 $toemails = array();
 
 $toemails[] = array(
-				'email' => 'ablount@gmail.com', // Your Email Address
+				'email' => 'hello@volunteerismabroad.com', // Your Email Address
 				'name' => 'Volunteerism Abroad' // Your Name
 			);
 
